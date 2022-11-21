751070cf4cdb90bac30f7d72dd31e706a5603fec
Midokhalil1 <midokhalil22@gmail.com>
2022-11-21 11:12:50 -0500

    insertone

--- dbConnect.js
+++ dbConnect.js
@@ -1,6 +1,6 @@
-import { MongoClient } from 'mongodb';
-import { uri } from "../credentials.js";
-export function dbConnect () {
-    const client = new MongoClient(uri);
-    return client.db("mydatabase");
-}
+// import { MongoClient } from 'mongodb';
+// import {MONGO_URI} from "../.env";
+// export function dbConnect () {
+//     const client = new MongoClient(uri);
+//     return client.db("mydatabase");
+// }
--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 import express from "express";
 import cors from "cors"
+import { MongoClient } from "mongodb";
+import 'dotenv/config'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
+const client = new MongoClient(process.env.MONGO_URI) // mongoclient is a class and we creating new instince of it 
+const database = client.db('blog')
+const posts = database.collection('posts')
+
+client.connect()
+console.log('connected to mongo')
 
 
-app.get('/', (req, res) => {
-    res.send('hello')
+
+// app.get('/', (req, res) => {
+//     res.send('hello')
+// })
+
+
+app.listen(process.env.PORT, () => console.log(`listening to http://localhost:${process.env.PORT}....`))
+
+// post - add one post item
+// ---- author, date, test 
+
+app.post('/', async (req, res) => {
+    const newPost = {author: 'Jon', date: '2022-21-22', text: 'lorem lorem here'}
+    console.log('req.body ->', req.body)
+
+    await posts.insertOne(req.body)
+    res.send('item add')
+
 })
-const PORT = 3030
 
-app.listen(PORT, () => console.log(`listening to http://localhost:${PORT}....`))
+
+
+
+// get - to get all post 
 
 
 
